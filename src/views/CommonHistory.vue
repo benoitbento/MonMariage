7131b72886fc0893690985ec20bf708d8e116997
benoitbento <40117945+benoitbento@users.noreply.github.com>
2026-01-07 21:22:57 +0100

    ajout du texte de la rencotre (#3)

--- src/views/CommonHistory.vue
+++ src/views/CommonHistory.vue
@@ -1,6 +1,14 @@
 <template><v-container>
         <h1>Notre histoire</h1>
-        TEST
+        Nous nous sommes rencontrés à Bordeaux, en 2019, au karaté. Benoît a été sonné par le mawashi-geri (coup de pieds circulaire) d’Elyne tandis qu’Elyne a été charmée par l’humour de Benoît, comme quoi, même dans un temple de la violence, le coup de foudre est à porter de pied. 
+
+Malheureusement, nous étions en 2020, autant vous rappeler que les relations sociales ont été quelque peu freinées à cause d’un pengolin. Notre premier rendez-vous a donc eu lieu à la sortie du premier confinement en mai 2020 : Benoît avait préparé un succulent repas puis programmé le film Le silence des agneaux (il avait tout compris...). 
+
+Habitant tous les deux à Bordeaux, nous en profitâmes pour visiter ses alentours, la Charente-Maritime, l’Eure-et-Loir mais aussi de succulents restaurants gastronomiques avec option panne de carte gold. 
+
+En 2022, après deux belles années de relation sans cohabitation, nous nous installâmes enfin ensemble et déménageâmes à Lille pour qu'Elyne puisse finir ses études, mais aussi vider Benoit de l’excès de vitamine D. Nous passâmes notre ceinture noire de karaté, de quoi rendre Elyne encore plus terrifiante.  
+
+Comme le Nord était synonyme d’étranger voire d’étrange, nous voyageâmes en Belgique, à Lisbonne, à Budapest et bien entendu dans les Hauts-de-France. Nous nous fiançâmes le 15 juin 2025 lors d’un week-end à Budapest pour aller voir Imagine Dragons en concert, après 3 tentatives avortées, la quatrième fut la bonne et Elyne a dit OUI.  
     </v-container></template>
 
 
