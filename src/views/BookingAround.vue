396af7263077141ed5d806218900b86ba11cf556
benoitbento <40117945+benoitbento@users.noreply.github.com>
2026-01-31 22:58:36 +0100

    clean adress page and booking around page (#15)

--- src/views/BookingAround.vue
+++ src/views/BookingAround.vue
@@ -1,4 +1,6 @@
 <template><wedding-card title="Confirmez votre présence">
+    Vous trouverez, ici, des adresses de lieux où vous pourrez dormir à proximité de l’Abbaye de Fontdouce :
+
     </wedding-card></template>
 <script>
 import WeddingCard from '@/components/WeddingCard.vue';
