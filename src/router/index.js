b6b304ae055316552bd02b5609becbbcb8978e76
benoitbento <40117945+benoitbento@users.noreply.github.com>
2026-04-24 18:58:40 +0200

    Feature/fixpipeline (#61) (#65)
    * Feature/fixpipeline (#61)
    
    * private the website
    
    * "try to correct yaml"
    
    * fill navbar
    
    * add index
    
    * add redirection to index

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
-// import CommonHistory from '@/views/CommonHistory.vue'
-// import AdressesWedding from '@/views/AdressesWedding.vue'
-// import BookingAround from '@/views/BookingAround.vue'
-// import DressCode from '@/views/DressCode.vue'
-// import PlanningWholeDay from '@/views/PlanningWholeDay.vue'
+import CommonHistory from '@/views/CommonHistory.vue'
+import AdressesWedding from '@/views/AdressesWedding.vue'
+import BookingAround from '@/views/BookingAround.vue'
+import DressCode from '@/views/DressCode.vue'
+import PlanningWholeDay from '@/views/PlanningWholeDay.vue'
 
 const routes = [
  { 
@@ -17,52 +17,52 @@ const routes = [
       description: 'Découvrez notre histoire d\'amour et les détails de notre mariage. Consultez le programme, le dress code, les adresses et les informations pratiques.'
     }
   }
-  // ,
-  // { 
-  //   path: '/CommonHistory', 
-  //   name: 'CommonHistory', 
-  //   component: CommonHistory, 
-  //   meta: { 
-  //     title: 'Notre Histoire | Mariage',
-  //     description: 'Découvrez comment nous nous sommes rencontrés et notre belle histoire d\'amour.'
-  //   }
-  // },
-  // { 
-  //   path: '/DressCode', 
-  //   name: 'DressCode', 
-  //   component: DressCode, 
-  //   meta: { 
-  //     title: 'Dress Code | Mariage',
-  //     description: 'Consultez nos recommandations de tenue pour notre mariage.'
-  //   }
-  // },
-  // { 
-  //   path: '/PlanningWholeDay', 
-  //   name: 'PlanningWholeDay', 
-  //   component: PlanningWholeDay, 
-  //   meta: { 
-  //     title: 'Programme du Jour | Mariage',
-  //     description: 'Découvrez le planning détaillé de notre journée de mariage.'
-  //   }
-  // },
-  // { 
-  //   path: '/BookingAround', 
-  //   name: 'BookingAround', 
-  //   component: BookingAround, 
-  //   meta: { 
-  //     title: 'Réservation & Alentours | Mariage',
-  //     description: 'Informations pratiques sur les réservations et les attractions aux alentours.'
-  //   }
-  // },
-  // { 
-  //   path: '/AdressesWedding', 
-  //   name: 'AdressesWedding', 
-  //   component: AdressesWedding, 
-  //   meta: { 
-  //     title: 'Adresses | Mariage',
-  //     description: 'Tous les détails et adresses pratiques pour notre mariage.'
-  //   }
-  // }
+  ,
+  { 
+    path: '/CommonHistory', 
+    name: 'CommonHistory', 
+    component: CommonHistory, 
+    meta: { 
+      title: 'Notre Histoire | Mariage',
+      description: 'Découvrez comment nous nous sommes rencontrés et notre belle histoire d\'amour.'
+    }
+  },
+  { 
+    path: '/DressCode', 
+    name: 'DressCode', 
+    component: DressCode, 
+    meta: { 
+      title: 'Dress Code | Mariage',
+      description: 'Consultez nos recommandations de tenue pour notre mariage.'
+    }
+  },
+  { 
+    path: '/PlanningWholeDay', 
+    name: 'PlanningWholeDay', 
+    component: PlanningWholeDay, 
+    meta: { 
+      title: 'Programme du Jour | Mariage',
+      description: 'Découvrez le planning détaillé de notre journée de mariage.'
+    }
+  },
+  { 
+    path: '/BookingAround', 
+    name: 'BookingAround', 
+    component: BookingAround, 
+    meta: { 
+      title: 'Réservation & Alentours | Mariage',
+      description: 'Informations pratiques sur les réservations et les attractions aux alentours.'
+    }
+  },
+  { 
+    path: '/AdressesWedding', 
+    name: 'AdressesWedding', 
+    component: AdressesWedding, 
+    meta: { 
+      title: 'Adresses | Mariage',
+      description: 'Tous les détails et adresses pratiques pour notre mariage.'
+    }
+  }
 ]
 
 const router = createRouter({
